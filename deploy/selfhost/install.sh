91a56a502666a9c68d49a227b77f68ebf674ccd5
Sarthak Bansal <sbansal793@gmail.com>
2024-04-25 16:28:38 +0530

    added

--- deploy/selfhost/install.sh
+++ deploy/selfhost/install.sh
@@ -56,7 +56,7 @@ function buildLocalImage() {
 
     if [ "$DO_BUILD" == "1" ] || [ "$DO_BUILD" == "" ];
     then
-        REPO=https://github.com/makeplane/plane.git
+        REPO=https://github.com/TheManhattanProject/plane.git
         CURR_DIR=$PWD
         PLANE_TEMP_CODE_DIR=$(mktemp -d)
         git clone $REPO $PLANE_TEMP_CODE_DIR  --branch $BRANCH --single-branch
@@ -96,8 +96,8 @@ function download() {
         mv $PLANE_INSTALL_DIR/docker-compose.yaml $PLANE_INSTALL_DIR/archive/$TS.docker-compose.yaml
     fi
 
-    curl -H 'Cache-Control: no-cache, no-store' -s -o $PLANE_INSTALL_DIR/docker-compose.yaml  https://raw.githubusercontent.com/makeplane/plane/$BRANCH/deploy/selfhost/docker-compose.yml?$(date +%s)
-    curl -H 'Cache-Control: no-cache, no-store' -s -o $PLANE_INSTALL_DIR/variables-upgrade.env https://raw.githubusercontent.com/makeplane/plane/$BRANCH/deploy/selfhost/variables.env?$(date +%s)
+    curl -H 'Cache-Control: no-cache, no-store' -s -o $PLANE_INSTALL_DIR/docker-compose.yaml  https://raw.githubusercontent.com/TheManhattanProject/plane/$BRANCH/deploy/selfhost/docker-compose.yml?$(date +%s)
+    curl -H 'Cache-Control: no-cache, no-store' -s -o $PLANE_INSTALL_DIR/variables-upgrade.env https://raw.githubusercontent.com/TheManhattanProject/plane/$BRANCH/deploy/selfhost/variables.env?$(date +%s)
 
     if [ -f "$DOCKER_ENV_PATH" ];
     then
